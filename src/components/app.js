1badf4b3c1feff54a87014ef18feeb2ad7fcc88b
Michael Ginalick <michaelginalick@Michaels-MacBook-Pro.local>
2017-12-31 15:42:47 -0600

    populate board

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,31 @@
 ((() => {
   const html = `
-    <div class="app">
-      <h1>Hello World</h1>
-      <!-- place your app components here -->
+    <div class="wrapper">
+      <div v-for="(tile, index) in tiles" v-bind:key="tile.id" class="box">{{tile.id}}</div>
     </div>
   `
 
   Vue.component("app", {
-    template: html
+    template: html,
+
+    data() {
+      return {
+        tiles: [],
+      }
+    },
+
+    mounted() {
+      this.populateBoard()
+    },
+
+
+    methods: {
+      populateBoard() {
+        for(let i = 0; i <= 23; i++) {
+          this.tiles.push({id: i})
+        }
+      }
+    }
+
   })
 }))()
